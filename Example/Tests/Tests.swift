73faa2a38438a72d0f96f8252d29bbfb8fc8014c
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-11-02 12:03:23 +0200

    [NEW]: (TESTS) - add simple unit test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,28 +1,12 @@
 import XCTest
-import CustomPod_Example_Axon
+@testable import CustomPod_Example_Axon
 
-class Tests: XCTestCase {
+class CocoaPodsSDKTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var calculator = Calculator(key: "GoodMood")
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+    func testMath() {
+        XCTAssertEqual(calculator.doMath(n1: 1, n2: 2, operation: .multiply), 2)
     }
     
 }
