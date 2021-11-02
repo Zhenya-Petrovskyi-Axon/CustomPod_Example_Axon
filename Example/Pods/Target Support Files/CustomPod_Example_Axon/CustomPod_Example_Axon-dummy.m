c3da26a4c89958d4b95dce5942379d87ee60ccf1
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-11-02 11:17:54 +0200

    Initial set up

---
+++ Example/Pods/Target Support Files/CustomPod_Example_Axon/CustomPod_Example_Axon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CustomPod_Example_Axon : NSObject
+@end
+@implementation PodsDummy_CustomPod_Example_Axon
+@end
---
+++ Example/Pods/Target Support Files/Pods-CustomPod_Example_Axon_Example/Pods-CustomPod_Example_Axon_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomPod_Example_Axon_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CustomPod_Example_Axon_Example
+@end
